d3d2f5cfcfc5510236616c0218064550c7a2ae9d
Guy Bedford <gbedford@cloudflare.com>
2026-02-03 13:22:35 -0800

    debugging

--- justfile
+++ justfile
@@ -61,7 +61,7 @@ test-wasm-bindgen-unwind *ARGS="":
     RUST_BACKTRACE=1 \
     WASM_BINDGEN_TEST_ONLY_NODE=1 \
     WASM_BINDGEN_SPLIT_LINKED_MODULES=1 \
-    cargo +nightly test \
+    cargo +nightly-2025-12-01 test \
         -Zbuild-std=std,panic_unwind \
         --target wasm32-unknown-unknown \
         {{ARGS}}
@@ -74,7 +74,7 @@ test-wasm-bindgen-unwind-reinit *ARGS="":
     RUST_BACKTRACE=1 \
     WASM_BINDGEN_TEST_ONLY_NODE=1 \
     WASM_BINDGEN_SPLIT_LINKED_MODULES=1 \
-    cargo +nightly test \
+    cargo +nightly-2025-12-01 test \
         -Zbuild-std=std,panic_unwind \
         --target wasm32-unknown-unknown \
         {{ARGS}} \
--- tests/wasm/unwind.rs
+++ tests/wasm/unwind.rs
@@ -55,65 +55,65 @@ macro_rules! js_array {
     })
 }
 
-#[wasm_bindgen_test]
-async fn try_promise_all() {
-    let mut resolve_ = Default::default();
-    let promise = Promise::new(&mut |resolve, _reject| {
-        resolve_ = resolve;
-    });
-    let closure1 = Closure::new(|_v| {
-        panic!("CLOSURE PANIC");
-    });
-    let promise2 = promise.then(&closure1);
-    let future = JsFuture::from(promise2);
-    let closure2 = Closure::new(move || {
-        resolve_
-            .call1(&JsValue::undefined(), &JsValue::undefined())
-            .unwrap();
-    });
-    set_timeout(&closure2);
-    let result = future.await;
-    assert!(result.is_err());
-    let err = result.err().unwrap();
-    let msg = Reflect::get(&err, &"message".into()).unwrap();
-    assert_eq!(msg, "CLOSURE PANIC");
-}
+// #[wasm_bindgen_test]
+// async fn try_promise_all() {
+//     let mut resolve_ = Default::default();
+//     let promise = Promise::new(&mut |resolve, _reject| {
+//         resolve_ = resolve;
+//     });
+//     let closure1 = Closure::new(|_v| {
+//         panic!("CLOSURE PANIC");
+//     });
+//     let promise2 = promise.then(&closure1);
+//     let future = JsFuture::from(promise2);
+//     let closure2 = Closure::new(move || {
+//         resolve_
+//             .call1(&JsValue::undefined(), &JsValue::undefined())
+//             .unwrap();
+//     });
+//     set_timeout(&closure2);
+//     let result = future.await;
+//     assert!(result.is_err());
+//     let err = result.err().unwrap();
+//     let msg = Reflect::get(&err, &"message".into()).unwrap();
+//     assert_eq!(msg, "CLOSURE PANIC");
+// }
 
-#[wasm_bindgen_test]
-fn try_every() {
-    let even = js_array![2, 4, 6, 8];
-    Reflect::set(&global(), &"dropped".into(), &JsValue::FALSE).unwrap();
-    Reflect::set(&global(), &"food".into(), &JsValue::FALSE).unwrap();
-    assert!(even
-        .try_every_result(&Closure::new(|_, _, _| {
-            struct Foo {}
-            impl Drop for Foo {
-                fn drop(&mut self) {
-                    Reflect::set(&global(), &"dropped".into(), &JsValue::TRUE).unwrap();
-                }
-            }
-            impl Foo {
-                fn foo(&self) {
-                    let _ = Reflect::set(&global(), &"food".into(), &JsValue::TRUE);
-                }
-            }
-            let foo = Foo {};
-            if std::hint::black_box(true) {
-                panic!("PANIC");
-            }
-            foo.foo();
-            Ok(true)
-        }))
-        .is_err());
-    assert!(!Reflect::get(&global(), &"food".into())
-        .unwrap()
-        .as_bool()
-        .unwrap());
-    assert!(Reflect::get(&global(), &"dropped".into())
-        .unwrap()
-        .as_bool()
-        .unwrap());
-}
+// #[wasm_bindgen_test]
+// fn try_every() {
+//     let even = js_array![2, 4, 6, 8];
+//     Reflect::set(&global(), &"dropped".into(), &JsValue::FALSE).unwrap();
+//     Reflect::set(&global(), &"food".into(), &JsValue::FALSE).unwrap();
+//     assert!(even
+//         .try_every_result(&Closure::new(|_, _, _| {
+//             struct Foo {}
+//             impl Drop for Foo {
+//                 fn drop(&mut self) {
+//                     Reflect::set(&global(), &"dropped".into(), &JsValue::TRUE).unwrap();
+//                 }
+//             }
+//             impl Foo {
+//                 fn foo(&self) {
+//                     let _ = Reflect::set(&global(), &"food".into(), &JsValue::TRUE);
+//                 }
+//             }
+//             let foo = Foo {};
+//             if std::hint::black_box(true) {
+//                 panic!("PANIC");
+//             }
+//             foo.foo();
+//             Ok(true)
+//         }))
+//         .is_err());
+//     assert!(!Reflect::get(&global(), &"food".into())
+//         .unwrap()
+//         .as_bool()
+//         .unwrap());
+//     assert!(Reflect::get(&global(), &"dropped".into())
+//         .unwrap()
+//         .as_bool()
+//         .unwrap());
+// }
 
 // #[wasm_bindgen_test]
 // fn try_every_aborting() {
