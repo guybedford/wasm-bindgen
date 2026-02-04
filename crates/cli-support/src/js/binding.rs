94abacf9ceda2fdf9c2733174a635d19d22c27b2
Hood Chatham <roberthoodchatham@gmail.com>
2026-02-04 14:55:46 -0800

    WIP

--- crates/cli-support/src/js/binding.rs
+++ crates/cli-support/src/js/binding.rs
@@ -1427,6 +1427,8 @@ fn instruction(
 
                 js.push(format!("{make_closure}({a}, {b}, wasm.{dtor}, {wrapper})"));
             } else {
+                // Borrowed closure - no destructor, but we add _wbg_cb_unref
+                // to invalidate the closure after use
                 let i = js.tmp();
                 js.prelude(&format!("var state{i} = {{a: {a}, b: {b}}};"));
                 let args = (0..*nargs)
@@ -1450,15 +1452,14 @@ fn instruction(
                     ));
                 } else {
                     js.prelude(&format!(
-                        "var cb{i} = ({args}) => {wrapper}(state{i}.a, state{i}.b, {args});",
+                        "var cb{i} = ({args}) => {{ return {wrapper}(state{i}.a, state{i}.b, {args}); }};",
                     ));
                 }
 
-                // Make sure to null out our internal pointers when we return
-                // back to Rust to ensure that any lingering references to the
-                // closure will fail immediately due to null pointers passed in
-                // to Rust.
-                js.finally(&format!("state{i}.a = state{i}.b = 0;"));
+                // Add _wbg_cb_unref to invalidate the borrowed closure
+                js.prelude(&format!(
+                    "cb{i}._wbg_cb_unref = () => {{ state{i}.a = state{i}.b = 0; }};",
+                ));
                 js.push(format!("cb{i}"));
             }
         }
