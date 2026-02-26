2a06e807caa302110ef3c842d7f9aaee56d4d621
magic-akari <akari.ccino@gmail.com>
2026-02-26 20:45:33 +0800

    feat: add support for `unchecked_optional_param_type`

--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -13,7 +13,7 @@ pub mod tys;
 // This gets changed whenever our schema changes.
 // At this time versions of wasm-bindgen and wasm-bindgen-cli are required to have the exact same
 // SCHEMA_VERSION in order to work together.
-pub const SCHEMA_VERSION: &str = "0.2.107";
+pub const SCHEMA_VERSION: &str = "0.2.108";
 
 #[macro_export]
 macro_rules! shared_api {
@@ -161,6 +161,7 @@ macro_rules! shared_api {
         struct FunctionArgumentData<'a> {
             name: String,
             ty_override: Option<&'a str>,
+            optional: bool,
             desc: Option<&'a str>,
         }
 
